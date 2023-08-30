95fd93fbfa6af47d5a7414a3be0bc2f07ed0332a
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-08-31 01:33:49 +0530

    feat: frontend complete fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logo from '../assets/logo.d4fbb84b.png'
+import logo from '../assets/newlogo.png'
 import { ethers } from 'ethers';
 import { useEffect, useLayoutEffect, useState } from 'react';
 // import Login from './login';
@@ -10,57 +10,48 @@ import { Web3Provider } from '@ethersproject/providers';
 
 
 
-const Header = () => {
-    const [address, setAddress] = useState(null)
+const Header = ({ accountAddress, setAccountAddress, chainId, setChainId }) => {
 
-    useLayoutEffect(() => {
-        const res = localStorage.getItem('address', address)
-        if (res) {
-            setAddress(res)
+    const reqAccounts = () => {
+        if (window.ethereum) {
+            // res[0] for fetching a first wallet
+            window.ethereum
+                .request({ method: "eth_requestAccounts" })
+                .then((res) => {
+                    setAccountAddress(res[0])
+                });
         }
+    }
+
+    useEffect(() => {
+        reqAccounts()
     }, [])
 
-    useLayoutEffect(() => {
-        if (address) {
-            localStorage.setItem('address', address)
-        }
-    }, [address])
+    window.ethereum.on('accountsChanged', () => reqAccounts());
 
-    async function handleMetaMaskLogin() {
-        let provider;
-        if (window.ethereum == null) {
-            console.log("MetaMask not installed; using read-only defaults")
-            provider = ethers.getDefaultProvider()
+    const btnhandler = () => {
+        // Asking if metamask is already present or not
+        if (window.ethereum) {
+            reqAccounts()
         } else {
-            provider = new ethers.BrowserProvider(window.ethereum)
-            let signer = await provider.getSigner();
-            setAddress(signer.address)
+            alert("install metamask extension!!");
         }
-
-    }
-    async function handleLogout() {
-        setAddress(null)
-        localStorage.removeItem('address');
-    }
+    };
 
     return (
         <>
-            <div className='flex m-3 ml-2'>
+            <div className='flex px-2 p-2 bg-black text-white  '>
                 <Link to='/'>
                     <img className='w-44 mx-3' src={logo} alt="logo" />
                 </Link>
                 {
-                    address ? <h5>Hello:{address}</h5> : <button onClick={handleMetaMaskLogin} className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] '>Login with MetaMask</button>
+                    accountAddress ? <h5 className='text-cyan-500 mt-7'>Hello:{accountAddress}</h5> : <button onClick={btnhandler} className='text-2xl text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg  px-5 py-2.5 text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] '>Login with MetaMask</button>
 
                 }
 
                 <Link to='/create'>
-                    <h5 className='mx-3 text-center text-sm hover:text-blue-400 pt-2'>Create-E-signature-request</h5>
+                    <h5 className=' mx-auto  py-5 px-5 ml-6 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] '>Create-E-signature-request</h5>
                 </Link>
-                <Link to='/history'>
-                    <h5 className='mx-3 text-center text-sm hover:text-blue-400 pt-2'>Lookup</h5>
-                </Link>
-                <button onClick={handleLogout} className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] '>Logout</button>
             </div >
         </>
     )
